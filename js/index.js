090fb66d45308a35e1726374e2300b48403e60ec
Mishomoon <mjafarmo25@students.oamk.fi>
2026-03-11 21:44:49 +0200

    part 5 done

--- js/index.js
+++ js/index.js
@@ -1,44 +1,52 @@
 const BACKEND_ROOT_URL = 'http://localhost:3001'
+import { Todos } from "./class/Todos.js"
+
+const todos = new Todos(BACKEND_ROOT_URL)
 
 const list = document.querySelector('ul')
 const input = document.querySelector('input')
 
 input.disabled = true
 
+const renderSpan = (li, text) => {
+  const span = li.appendChild(document.createElement('span'))
+  span.innerHTML = text
+}
+
+const renderLink = (li, id) => {
+  const a = li.appendChild(document.createElement('a'))
+  a.innerHTML = '<i class="bi bi-trash"></i>'
+  a.setAttribute('style','float: right')
+  a.addEventListener('click', (event) => {
+    todos.removeTask(id).then((removed_id) => {
+      const li_to_remove = document.querySelector(`[data-key='${removed_id}']`)
+      if (li_to_remove) {
+        list.removeChild(li_to_remove)
+      }
+    }).catch((error) => {
+      alert(error)
+    })
+  })
+}
+
 const renderTask = (task) => {
   const li = document.createElement('li')
   li.setAttribute('class', 'list-group-item')
-  li.innerHTML = task
+  li.setAttribute('data-key', task.getId().toString())
+  renderSpan(li, task.getText())
+  renderLink(li, task.getId())
   list.append(li)
 }
 
-const getTasks = async () => {
-  try {
-    const response = await fetch(BACKEND_ROOT_URL)
-    const json = await response.json()
-    json.forEach(task => {
-      renderTask(task.description)
+const getTasks = () => {
+  todos.getTasks().then((tasks) => {
+    tasks.forEach(task => {
+      renderTask(task)
     })
     input.disabled = false
-  } catch (error) {
-    alert("Error retrieving tasks " + error.message)
-  }
-}
-
-const saveTask = async (task) => {
-  try {
-    const json = JSON.stringify({description: task})
-    const response = await fetch(BACKEND_ROOT_URL + '/new', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: json
-    })
-    return response.json()
-  } catch (error) {
-    alert("Error saving task " + error.message)
-  }
+  }).catch((error) => {
+    alert(error)
+  })
 }
 
 input.addEventListener('keypress', (event) => {
@@ -46,9 +54,10 @@ input.addEventListener('keypress', (event) => {
     event.preventDefault()
     const task = input.value.trim()
     if (task !== '') {
-      saveTask(task).then((json) => {
+      todos.addTask(task).then((task) => {
         renderTask(task)
         input.value = ''
+        input.focus()
       })
     }
   }
