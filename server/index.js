59b3caca7c3a73297577cbdabcfbdd0d7254866c
Mishomoon <mjafarmo25@students.oamk.fi>
2026-03-13 22:10:17 +0200

    part 8 deploy done

--- server/index.js
+++ server/index.js
@@ -9,6 +9,13 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use('/', todoRouter)
 
+
+await pool.query(`
+CREATE TABLE IF NOT EXISTS task (
+  id SERIAL PRIMARY KEY,
+  description VARCHAR(255) NOT NULL
+)
+`);
 const port = process.env.PORT
 
 app.listen(port, () => {
